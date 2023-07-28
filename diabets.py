27dfdc15764cebefa6a220a7abfe3777306d8c2c
batuhanumay <47949233+BatuhanUmay@users.noreply.github.com>
2023-07-28 12:43:41 +0300

    info added

--- diabets.py
+++ diabets.py
@@ -31,13 +31,12 @@ def check_df(dataframe, head=5):
     print(dataframe.dtypes)
     print("##################### Head #####################")
     print(dataframe.head(head))
-    print("##################### Tail #####################")
-    print(dataframe.tail(head))
+    print("##################### Info #####################")
+    print(dataframe.info())
     print("##################### NA #####################")
     print(dataframe.isnull().sum())
     print("##################### Quantiles #####################")
     print(dataframe.describe([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
-
 
 check_df(df)
 
