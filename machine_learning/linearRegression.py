a98d15ef06db02e3968f56fa46f9ac8b3f9be405
batuhanumay <47949233+BatuhanUmay@users.noreply.github.com>
2023-08-05 14:32:11 +0300

    machine learning linear regression

--- machine_learning/linearRegression.py
+++ machine_learning/linearRegression.py
@@ -13,6 +13,10 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.model_selection import train_test_split, cross_val_score
 
+import matplotlib;
+
+matplotlib.use('Qt5Agg')
+
 ######################################################
 # Simple Linear Regression with OLS Using Scikit-Learn
 ######################################################
@@ -152,8 +156,89 @@ np.mean(np.sqrt(-cross_val_score(reg_model,
                                  cv=5,
                                  scoring="neg_mean_squared_error")))
 
+
 ######################################################
 # Simple Linear Regression with Gradient Descent from Scratch
 ######################################################
 
+# Cost function MSE
+def cost_function(Y, b, w, X):
+    m = len(Y)
+    sse = 0  # sum of squared error
 
+    for i in range(m):
+        y_hat = b + w * X[i]
+        y = Y[i]
+        sse += (y_hat - y) ** 2
+
+    mse = sse / m
+    return mse
+
+
+"""
+COST FUNCTION
+-------------
+J(Q0, Q1) = (1 / 2m) * i=1,m [[hQ(xi) - yi] ** 2] # hQ(xi) = predicted value , yi = True value
+
+Q0 = Q0 - alpha * (1/m) * i=1,m[(hQ(xi) - yi)] # SABİT İÇİN TÜREV
+Q1 = Q1 - alpha * (1/m) * i=1,m[(hQ(xi) - yi).xi] # AĞIRLIK İÇİN TÜREV
+"""
+
+
+# update_weights
+def update_weights(Y, b, w, X, learning_rate):
+    m = len(Y)
+    b_deriv_sum = 0
+    w_deriv_sum = 0
+
+    for i in range(m):
+        y_hat = b + w * X[i]
+        y = Y[i]
+        b_deriv_sum += (y_hat - y)
+        w_deriv_sum += (y_hat - y) * X[i]
+
+    new_b = b - (learning_rate * 1 / m * b_deriv_sum)
+    new_w = w - (learning_rate * 1 / m * w_deriv_sum)
+
+    return new_b, new_w
+
+
+"""
+batch gradient descent = bütün gözlem birimlerine gidilir
+stokastik gradient descent = her iterasyonda bir gözlem birimine gidilir
+mini batch gradient descent = 5,10,100 şeklinde farklı sayıda gözlem birimine gidilir
+"""
+
+
+# train fonksiyonu
+def train(Y, initial_b, initial_w, X, learning_rate, num_iters):
+    print("Starting gradient descent at b = {0}, w = {1}, mse = {2}".format(initial_b, initial_w,
+                                                                            cost_function(Y, initial_b, initial_w, X)))
+    b = initial_b
+    w = initial_w
+    cost_history = []
+
+    for i in range(num_iters):
+        b, w = update_weights(Y, b, w, X, learning_rate)
+        mse = cost_function(Y, b, w, X)
+        cost_history.append(mse)
+
+        if i % 100 == 0:
+            print("iter={:d}    b={:.2f}    w={:.4f}    mse={:.4}".format(i, b, w, mse))
+
+    print("After {0} iterations b = {1}, w = {2}, mse = {3}".format(num_iters, b, w, cost_function(Y, b, w, X)))
+    return cost_history, b, w
+
+
+df = pd.read_csv("datasets/advertising.csv")
+
+X = df["radio"]
+Y = df["sales"]
+
+# hyperparameters
+learning_rate = 0.001
+initial_b = 0.001
+initial_w = 0.001
+num_iters = 100000
+
+cost_history, b, w = train(Y, initial_b, initial_w, X, learning_rate, num_iters)
